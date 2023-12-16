d350d6cc575d29d077458d96d64a476bfd123991
MartinVazquez1982 <martin.vazquezaris@gmail.com>
2023-12-16 12:28:37 -0300

    implementacion de primeros cambios en nnUNetTrainerAneurysm, solamente algunos parametros del constructor
    Lo que se hizo que se llame al construcctor del padre y que luego se modifiquen el numero de epocas, el numero de iteraciones por epoca y el numero de iteraciones de validación por epoca

--- nnunetv2/training/nnUNetTrainer/variants/Aneurysms/nnUNetTrainerAneurysm.py
+++ nnunetv2/training/nnUNetTrainer/variants/Aneurysms/nnUNetTrainerAneurysm.py
@@ -4,5 +4,19 @@ from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 
 class nnUNetTrainerAneurysm(nnUNetTrainer):
     
-    def _init_(self):
-        sinError=None
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        
+        #Este parámetro define directamente el número de veces que la red recorrerá todo el conjunto de datos de entrenamiento.
+        self.num_epochs = 200 #Por defecto 1000
+        
+        # Este parámetro define el número de iteraciones de entrenamiento por época. 
+        # Sin embargo, el tamaño del lote real se calcula indirectamente dividiendo este número por el número de muestras de entrenamiento 
+        # en su conjunto de datos. Por ejemplo, si tiene 10.000 muestras de entrenamiento, el tamaño del lote sería 10.000 / 250 = 40.
+        self.num_iterations_per_epoch = 250 #Por defecto 250
+        
+        # Este parámetro funciona de manera similar al anterior pero determina el número de iteraciones de validación por época, 
+        # definiendo indirectamente el tamaño del lote de validación
+        self.num_val_iterations_per_epoch = 50 #Por defecto 50
+        
+        
