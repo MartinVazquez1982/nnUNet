931d28704389fea0d82d175cf1adb537b87f42cd
MartinVazquez1982 <martin.vazquezaris@gmail.com>
2023-12-27 12:26:01 -0300

    finalizacion de metodo get_training_transforms con minimas transformaciones

--- nnunetv2/training/nnUNetTrainer/variants/Aneurysms/nnUNetTrainerAneurysm.py
+++ nnunetv2/training/nnUNetTrainer/variants/Aneurysms/nnUNetTrainerAneurysm.py
@@ -3,8 +3,11 @@ import numpy as np
 
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from typing import List, Union, Tuple
-from batchgenerators.transforms.abstract_transforms import AbstractTransform
-
+from batchgenerators.transforms.abstract_transforms import AbstractTransform, Compose
+from batchgenerators.transforms.utility_transforms import NumpyToTensor, RenameTransform, RemoveLabelTransform
+from nnunetv2.training.data_augmentation.custom_transforms.deep_supervision_donwsampling import \
+    DownsampleSegForDSTransform2
+from batchgenerators.transforms.spatial_transforms import SpatialTransform
 
 class nnUNetTrainerAneurysm(nnUNetTrainer):
     
@@ -31,6 +34,7 @@ class nnUNetTrainerAneurysm(nnUNetTrainer):
         # # definiendo indirectamente el tamaño del lote de validación
         # self.num_val_iterations_per_epoch = 50 #Por defecto 50
     
+    @staticmethod
     def get_training_transforms(patch_size: Union[np.ndarray, Tuple[int]],
                                 rotation_for_DA: dict,
                                 deep_supervision_scales: Union[List, Tuple, None],
@@ -45,17 +49,29 @@ class nnUNetTrainerAneurysm(nnUNetTrainer):
                                 regions: List[Union[List[int], Tuple[int, ...], int]] = None,
                                 ignore_label: int = None) -> AbstractTransform:
         
-        matching_axes = np.array([sum([i == j for j in patch_size]) for i in patch_size])
-        valid_axes = list(np.where(matching_axes == np.max(matching_axes))[0])
+        tr_transforms = []       
         
-        tr_transforms = []
+        tr_transforms.append(SpatialTransform(
+            patch_size, patch_center_dist_from_border=None,
+            do_elastic_deform=False, alpha=(0, 0), sigma=(0, 0),
+            do_rotation=True, angle_x=rotation_for_DA['x'], angle_y=rotation_for_DA['y'], angle_z=rotation_for_DA['z'],
+            p_rot_per_axis=1,  # todo experiment with this
+            do_scale=True, scale=(0.7, 1.4),
+            border_mode_data="constant", border_cval_data=0, order_data=order_resampling_data,
+            border_mode_seg="constant", border_cval_seg=border_val_seg, order_seg=order_resampling_seg,
+            random_crop=False,  # random cropping is part of our dataloaders
+            p_el_per_sample=0, p_scale_per_sample=0.2, p_rot_per_sample=0.2,
+            independent_scale_for_each_axis=False  # todo experiment with this
+        ))
         
-        if do_dummy_2d_data_aug:
-            ignore_axes = (0,)
-            tr_transforms.append(Convert3DTo2DTransform())
-            patch_size_spatial = patch_size[1:]
-        else:
-            patch_size_spatial = patch_size
-            ignore_axes = None
+        tr_transforms.append(RemoveLabelTransform(-1, 0))
+
+        tr_transforms.append(RenameTransform('seg', 'target', True))
+
+        if deep_supervision_scales is not None:
+            tr_transforms.append(DownsampleSegForDSTransform2(deep_supervision_scales, 0, input_key='target',
+                                                              output_key='target'))
+            
+        tr_transforms.append(NumpyToTensor(['data', 'target'], 'float'))
         
-        
+        return Compose(tr_transforms)
