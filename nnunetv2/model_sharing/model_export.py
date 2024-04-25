27a88c345b893092a0eec522f0629e265ae875a6
MartinVazquez1982 <martin.vazquezaris@gmail.com>
2024-04-25 15:50:54 -0300

    main export model

--- nnunetv2/model_sharing/model_export.py
+++ nnunetv2/model_sharing/model_export.py
@@ -121,4 +121,12 @@ def export_pretrained_model(dataset_name_or_id: Union[int, str], output_file: st
 
 
 if __name__ == '__main__':
-    export_pretrained_model(2, '/home/fabian/temp/dataset2.zip', strict=False, export_crossval_predictions=True, folds=(0, ))
+    export_pretrained_model( 
+        dataset_name_or_id=1, 
+        output_file='', 
+        configurations=("3d_fullres_Aneurysm", ), 
+        trainer='nnUNetTrainerAneurysm',
+        folds=(0, ),
+        strict=False,
+        export_crossval_predictions=True
+    )
